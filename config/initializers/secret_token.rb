abc2a0a6a954b3ee6078a60ac865b0a9e3f6642a
Andrew Havens <andrewh@copiousinc.com>
2013-10-18 22:03:42 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubygamedev::Application.config.secret_key_base = '467c33ca9481c483179ae5a517d6530d64e5b31c6719de4e509e8f882b870008d79adcce034c70fc5cdd4ab102f949bad2134a9a3bb5d91332052ff2e2b71652'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rubygamedev::Application.config.session_store :cookie_store, key: '_rubygamedev_session'
