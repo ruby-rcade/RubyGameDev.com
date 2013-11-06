a826189e1afd3da1c952621c00d8581ee17966d3
Andrew Havens <andrewh@copiousinc.com>
2013-11-06 00:09:21 -0800

    add remaining methods

--- app/models/digest_history.rb
+++ app/models/digest_history.rb
@@ -5,12 +5,20 @@ class DigestHistory < ActiveRecord::Base
     history.last_discussion_id = Discussion.last.id
     history.last_tutorial_id = Tutorial.last.id
     history.last_library_id = Library.last.id
-    history.user_count = User.count
+    history.users_count = User.count
     history.save!
   end
 
   def discussions
     Discussion.where('id > ?', last_discussion_id)
+  end
+
+  def tutorials
+    Tutorial.where('id > ?', last_discussion_id)
+  end
+
+  def libraries
+    Tutorial.where('id > ?', last_discussion_id)
   end
 
 end
