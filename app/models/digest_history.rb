9dbdc19485057fd76944480355a99d86fa35630a
Andrew Havens <andrewh@copiousinc.com>
2013-11-06 00:06:07 -0800

    Adds discussions to digest

--- app/models/digest_history.rb
+++ app/models/digest_history.rb
@@ -2,11 +2,15 @@ class DigestHistory < ActiveRecord::Base
 
   def self.create_new!
     history = new
-    # history.last_discussion_id = Forum::Discussion.last.id
+    history.last_discussion_id = Discussion.last.id
     history.last_tutorial_id = Tutorial.last.id
     history.last_library_id = Library.last.id
     history.user_count = User.count
     history.save!
+  end
+
+  def discussions
+    Discussion.where('id > ?', last_discussion_id)
   end
 
 end
