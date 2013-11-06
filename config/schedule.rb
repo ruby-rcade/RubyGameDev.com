cf0ad168998418a4fa409b816bb4453a4c1022f3
Andrew Havens <andrewh@copiousinc.com>
2013-11-06 15:42:47 -0800

    Add crons to check env

--- config/schedule.rb
+++ config/schedule.rb
@@ -29,9 +29,9 @@ every 1.hour do
   runner 'DigestBuilder.send_daily_email'
 end
 
-every :minute do
+every 1.minute do
   command "whoami"
 end
-every :minute do
+every 1.minute do
   command "which ruby"
 end
