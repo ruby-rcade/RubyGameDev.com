1feb43c0f99e8c0638eee8e8d07809bee2ee3c13
Andrew Havens <andrewh@copio.us>
2015-01-05 11:03:10 -0800

    Fix bug with nil user.

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,10 +1,12 @@
 PostPolicy = Struct.new(:user, :post) do
 
   def update?
+    return false unless user
     user.admin? or post.user == user
   end
 
   def destroy?
+    return false unless user
     user.admin?
   end
 end
