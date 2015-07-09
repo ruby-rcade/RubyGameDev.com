df72039397bb2ce0c814e47dd49e4d854d21282d
Boryana Manolova <b.manolova@yahoo.com>
2015-07-09 12:39:42 +0300

    Add Capybara tool to the project.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,8 +3,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
