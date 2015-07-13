5f38bce4f5460ba3ee706019e96e30bafff53ab2
Boryana Manolova <b.manolova@yahoo.com>
2015-07-13 10:55:07 +0300

    deleted by mistake :)

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,22 +1,23 @@
 require "rails_helper"
 
 describe "Home page" do
-	before do
-		visit '/'
-	end
+  before do
+    visit '/'
+  end
 
-	it "has a link redirecting to About page" do
-		click_link 'About RubyGameDev.com'
-		expect(current_path).to eq '/about'
-	end
+  it "has a link redirecting to About page" do
+    click_link 'About RubyGameDev.com'
+    expect(current_path).to eq '/about'
+  end
 
-	it "has a link redirecting to signup page" do
-	 	click_link 'Sign in'
-	 	expect(current_path).to eq '/sign_in'
-	end
+  it "has a link redirecting to signup page" do
+    click_link 'Sign in'
+    expect(current_path).to eq '/sign_in'
+  end
 
-	it "has a link to create a new post, which redirects to the signup page" do
-	 	click_link 'New'
-	 	expect(current_path).to eq '/sign_in'
-	end
+  context "link to create a new post"
+   it "redirecting to the signup page" do
+    click_link 'New'
+    expect(current_path).to eq '/sign_in'
+  end
 end
