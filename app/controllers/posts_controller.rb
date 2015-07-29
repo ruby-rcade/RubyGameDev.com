dfd3f6453ea1d324c2adfb4cef854f5edfde433d
Boryana Manolova <b.manolova@yahoo.com>
2015-07-29 14:48:17 +0300

    Minor bug fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-      @posts = Post.order('created_at desc')
-    end
+    @posts = Post.order('created_at desc')
   end
 
   # GET /posts/1
