5954a512513c07b2502360dff41c659ccf17f082
KarenVentura <powercodegirls@gmail.com>
2015-08-04 11:31:29 -0500

    improve code

--- app/workers/gamedev_worker.rb
+++ app/workers/gamedev_worker.rb
@@ -3,16 +3,19 @@ class GamedevWorker
 
   def perform
     response = RubyStackoverflow.questions({ order: 'asc', filter: 'withbody', pagesize: 100 })
-    response.data.each do | question|
-      p = ExternalPost.new(title: question.title,
+    response.data.each do |question|
+      p = ExternalPost.new(
+        title: question.title,
         user_display_name: question.owner[:display_name],
         body_html: question.body,
         source_url: question.link,
         external_id: question.question_id)
-		  p.save
+	    p.save
 	  end
   end
 
-  Sidekiq::Cron::Job.create(name: "RubyStackoverflow fetch questions every friday",
-    cron: "* * * * 5", klass: "GamedevWorker")
+  Sidekiq::Cron::Job.create(
+    name: "RubyStackoverflow fetch questions every friday",
+    cron: "* * * * 5",
+    klass: "GamedevWorker")
 end
