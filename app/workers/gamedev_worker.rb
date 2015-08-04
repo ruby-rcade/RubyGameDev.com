bad19cc356e547796bced8a6febdd70e726b8fb9
KarenVentura <powercodegirls@gmail.com>
2015-08-04 11:22:29 -0500

    Improve code

--- app/workers/gamedev_worker.rb
+++ app/workers/gamedev_worker.rb
@@ -2,9 +2,9 @@ class GamedevWorker
   include Sidekiq::Worker
 
   def perform
-	  response = RubyStackoverflow.questions({ order: 'asc', filter: 'withbody', pagesize: 100 })
-	  response.data.each do | question|
-		  p = ExternalPost.new(title: question.title,
+    response = RubyStackoverflow.questions({ order: 'asc', filter: 'withbody', pagesize: 100 })
+    response.data.each do | question|
+      p = ExternalPost.new(title: question.title,
         user_display_name: question.owner[:display_name],
         body_html: question.body,
         source_url: question.link,
