ea3e1a08bce9b6e9dd3132774ba3876f13366208
KarenVentura <powercodegirls@gmail.com>
2015-08-04 19:19:19 -0500

    Testing mailer notification

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,6 +8,6 @@ class Comment < ActiveRecord::Base
   after_create :notification_mailer
   
   def notification_mailer
-    CommentsMailer.notification_new_comment(self.parent.user_id).deliver!
+    CommentMailer.notification_new_comment(self.parent.user_id).deliver!
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,3 +5,4 @@ FactoryGirl.define do
     password 'password'
   end
 end
+	
