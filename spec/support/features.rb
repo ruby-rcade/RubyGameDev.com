45e005ffb67d1c3a98b8a60b961a4592371e7a07
Boryana Manolova <b.manolova@yahoo.com>
2015-08-07 09:29:37 +0300

    Votes styling

---
+++ spec/support/features.rb
@@ -0,0 +1,14 @@
+module Support
+ module Features
+   def create_and_sign_in_user
+     user = FactoryGirl.create(:user)
+
+     visit '/sign_in'
+     fill_in 'Email', with: user.email
+     fill_in 'Password', with: user.password
+     click_button 'Sign in'
+
+     user
+   end
+ end
+end
