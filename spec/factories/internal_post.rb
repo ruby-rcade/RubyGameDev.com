41686369885b77f23f742e821ee85e943e48495b
KarenVentura <powercodegirls@gmail.com>
2015-08-07 18:30:25 -0500

    add tests

--- spec/factories/internal_post.rb
+++ spec/factories/internal_post.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :internal_post do
     user
+    tags {[FactoryGirl.create(:tag)]}
     title "Example Discussion"
     body_markdown "Example body. So **strong**."
   end
---
+++ spec/helpers/post_location_helper_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+describe PostLocationHelper, :type => :helper do
+  describe "#link_to_post" do
+    it "returns gamedev url" do
+      post = FactoryGirl.create(:external_post)
+      expect(helper.link_to_post(post)).to  match("http://gamedev.stackexchange.com/")
+    end
+
+    it "returns posts url" do
+      post = FactoryGirl.create(:internal_post)
+      expect(helper.link_to_post(post)).to  match("/posts/")
+    end
+  end
+
+  describe "#show_tags" do
+    it "return questions tags" do
+      post = FactoryGirl.create(:external_post)
+      binding.pry
+      expect(helper.show_tags(post)).to  match("question")
+    end
+
+    it "return ruby tag" do
+      post = FactoryGirl.create(:internal_post)
+      binding.pry
+      expect(helper.show_tags(post)).to  match("example")
+    end
+  end
+end
