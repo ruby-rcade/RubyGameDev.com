3e59b74c323329b5bf9d14b0d0cd0bc6d6b35de0
Boryana Manolova <b.manolova@yahoo.com>
2015-08-07 09:29:37 +0300

    Votes styling

---
+++ spec/features/vote_page_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "Vote creating" do
+  before do
+    # Sign in
+    @user = create_and_sign_in_user
+    @post = FactoryGirl.create(:post)
+
+    # Go to account page
+    visit '/'
+  end
+
+  it "allows user to vote on a certain post" do
+    find('.vote-count a').click
+    expect(page).to have_content 'You have successfully voted'
+  end
+
+  it "doesn't allow user to vote again on the same post" do
+    expect(page).to have_css('.warning')
+  end
+end
