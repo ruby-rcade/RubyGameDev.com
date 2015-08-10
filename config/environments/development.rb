983f7f41063e0d135e1d260c2a85e58db039570b
KarenVentura <powercodegirls@gmail.com>
2015-08-10 16:06:15 -0500

    change config mailer in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,14 +18,11 @@ Rubygamedev::Application.configure do
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "localhost",
     port: 25,
     domain: "gmail.com",
-    user_name: "powercodegirls@gmail.com",
-    password: "powercode123",dig 
     authentication: "plain",
-    enable_starttls_auto: true,
-    openssl_verify_mode: "none"
+    enable_starttls_auto: true
   }
 
   # Print deprecation notices to the Rails logger.
