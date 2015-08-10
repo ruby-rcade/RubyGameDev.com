7cadd964454e3a8505d537c7edbb1d647bbfc812
Boryana Manolova <b.manolova@yahoo.com>
2015-08-10 17:07:31 +0300

    'houndci' fixes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,6 @@ class Vote < ActiveRecord::Base
   belongs_to :post
 
   validates_presence_of :user, :post
-  validates :user_id, uniqueness: { scope: :post_id, message: "has already voted once on this Post" }
+  validates :user_id, uniqueness: { scope: :post_id,
+    message: "has already voted once on this Post" }
 end
