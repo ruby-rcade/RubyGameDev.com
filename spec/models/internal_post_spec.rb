a41c3e5000b37e2fcc74002ad8ba6917fc2c90af
KarenVentura <powercodegirls@gmail.com>
2015-08-10 15:36:28 -0500

    checks if a tag title already exists

--- spec/models/internal_post_spec.rb
+++ spec/models/internal_post_spec.rb
@@ -12,44 +12,44 @@ describe InternalPost do
   it { should validate_presence_of :user }
   it { should validate_presence_of :title }
 
-  # describe "#notify_twitter" do
-  #   subject { FactoryGirl.build(:internal_post) }
+  describe "#notify_twitter" do
+    subject { FactoryGirl.build(:internal_post) }
 
-  #   it "posts to Twitter after create" do
-  #     expect(subject).to receive(:notify_twitter)
-  #     subject.save!
-  #   end
-  # end
+    it "posts to Twitter after create" do
+      expect(subject).to receive(:notify_twitter)
+      subject.save!
+    end
+  end
 
-  # describe "#tweet_content" do
-  #   subject { FactoryGirl.build(:internal_post) }
+  describe "#tweet_content" do
+    subject { FactoryGirl.build(:internal_post) }
 
-  #   it "includes the title in the tweet" do
-  #     subject.id = "123"
-  #     subject.title = "This is a test discussion"
-  #     expect(subject.tweet_content).to match /^This is a test discussion/
-  #   end
+    it "includes the title in the tweet" do
+      subject.id = "123"
+      subject.title = "This is a test discussion"
+      expect(subject.tweet_content).to match /^This is a test discussion/
+    end
 
-  #   it "uses the short domain for links" do
-  #     subject.id = "123"
-  #     expect(subject.tweet_content).to match %r{http://rbga.me/123$}
-  #   end
+    it "uses the short domain for links" do
+      subject.id = "123"
+      expect(subject.tweet_content).to match %r{http://rbga.me/123$}
+    end
 
-  #   it "keeps the character limit to 140" do
-  #     subject.id = "123"
-  #     subject.title = "a" * 140
-  #     expect(subject.tweet_content.length).to eq 140
-  #     expect(
-  #       subject.tweet_content
-  #     ).to eq "#{'a' * 121} http://rbga.me/123"
+    it "keeps the character limit to 140" do
+      subject.id = "123"
+      subject.title = "a" * 140
+      expect(subject.tweet_content.length).to eq 140
+      expect(
+        subject.tweet_content
+      ).to eq "#{'a' * 121} http://rbga.me/123"
 
-  #     subject.id = "1234567890"
-  #     expect(subject.tweet_content.length).to eq 140
-  #     expect(
-  #       subject.tweet_content
-  #     ).to eq "#{'a' * 114} http://rbga.me/1234567890"
-  #   end
-  # end
+      subject.id = "1234567890"
+      expect(subject.tweet_content.length).to eq 140
+      expect(
+        subject.tweet_content
+      ).to eq "#{'a' * 114} http://rbga.me/1234567890"
+    end
+  end
 
   describe "#username" do
     let(:internal_post) { FactoryGirl.create(:internal_post) }
@@ -110,7 +110,8 @@ describe InternalPost do
     end
 
     it "checks if a tag title already exists in other posts" do
-      @post2 = FactoryGirl.create :internal_post
+      tag = FactoryGirl.create :tag, title: 'otro_title'
+      @post2 = FactoryGirl.create :internal_post, tags: [tag]
 
       @post.tags_string = "ruby, great"
       @post.create_tags_from_tag_string
@@ -119,7 +120,7 @@ describe InternalPost do
       @post2.create_tags_from_tag_string
 
       all_tag_titles = Tag.all.map(&:title)
-      expect(all_tag_titles).to match_array ["ruby", "rails", "great"]
+      expect(all_tag_titles).to match_array ["example", "ruby", "rails", "great", "otro_title"]
     end
   end
 end
