7c4be283b5e32884e7ec25b485eda18292b54fed
Boryana Manolova <b.manolova@yahoo.com>
2015-08-11 08:54:31 +0300

    Fixing a long line

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,8 @@ class Vote < ActiveRecord::Base
   belongs_to :post
 
   validates_presence_of :user, :post
-  validates :user_id, uniqueness: { scope: :post_id,
-    message: "has already voted once on this Post" }
+  validates :user_id, uniqueness: {
+    scope: :post_id,
+    message: "has already voted once on this Post"
+  }
 end
