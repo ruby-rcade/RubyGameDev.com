7db658555be0dbd3855a9f231cefc48cd9f05f4a
KarenVentura <powercodegirls@gmail.com>
2015-08-11 19:07:06 -0500

    Improve code

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   has_and_belongs_to_many :tags
-  validates_presence_of :title
+  validates :title, presence: true
 
   scope :internal_post, -> { where(type: "InternalPost") }
   scope :external_post, -> { where(type: "ExternalPost") }
