9c5323debe04b8c6588b3f40f84e715c70fabacd
KarenVentura <powercodegirls@gmail.com>
2015-08-12 18:39:27 -0500

    WIP design email notification

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -3,8 +3,13 @@ class CommentMailer < ApplicationMailer
     @comment = Comment.find comment_id
     @user = User.find @comment.parent.user_id
     @parent_id = @comment.parent_id
+    @post_name = InternalPost.find @parent_id
     @url = "https://www.rubygamedev.com/internal_posts/#{@parent_id}"
 
+    attachments.inline['reply-button.png'] = File.read("#{Rails.root}/app/assets/images/reply-button.png")
+    attachments.inline['banner-header.png'] = File.read("#{Rails.root}/app/assets/images/banner-header.png")
+    attachments.inline['footer.png'] = File.read("#{Rails.root}/app/assets/images/footer.png")
     mail(to: @user.email, subject: "Notification comment")
   end
+
 end
