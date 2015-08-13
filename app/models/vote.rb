579a01462004a6dac8f103052c8f15a7cc3cc38a
Sophia Takeva <sofy@bitex.bg>
2015-08-13 10:26:28 +0300

    houndci fixes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  validates_presence_of :user, :post
+  validates :user, :post, presence: true
   validates :user_id, uniqueness: {
     scope: :post_id,
     message: "has already voted once on this Post"
