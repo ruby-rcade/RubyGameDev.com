7983de82745a43a23953764ad3339dc9bc3e7441
sandramu26 <san.muva@hotmail.com>
2015-08-17 17:49:55 -0500

    Fix external post tags

--- app/helpers/post_location_helper.rb
+++ app/helpers/post_location_helper.rb
@@ -10,12 +10,12 @@ module PostLocationHelper
   def show_votes(post)
       if post.type == "InternalPost"
       if post.has_voted?(current_user)
-        ("<span class='glyphicon glyphicon-chevron-up'></span><br/>" +
+        ("<span class='fa fa-chevron-up'></span><br/>" +
         "<span> #{post.votes.count} </span><br/>" +
         "<span class='small'>votes</span>").html_safe
       else
         link_to vote_post_path(post), method: :put do
-        ("<span class='glyphicon glyphicon-chevron-up'></span><br/>" +
+        ("<span class='fa fa-chevron-up'></span><br/>" +
         "<span> #{post.votes.count} </span><br/>" +
         "<span class='small'>votes</span>").html_safe
         end
