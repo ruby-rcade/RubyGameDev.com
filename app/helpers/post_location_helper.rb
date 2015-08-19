6bdec1c184171269e1a6117cd7dd961fe1603781
KarenVentura <powercodegirls@gmail.com>
2015-08-19 18:02:27 -0500

    Allows Internal and External post to have votes

--- app/helpers/post_location_helper.rb
+++ app/helpers/post_location_helper.rb
@@ -8,7 +8,6 @@ module PostLocationHelper
   end
 
   def show_votes(post)
-    if post.internal_post?
       if post.has_voted?(current_user)
         ("<span class='fa fa-chevron-up'></span><br/>" +
         "<span> #{post.votes.count} </span><br/>" +
@@ -20,6 +19,5 @@ module PostLocationHelper
           "<span class='small'>votes</span>").html_safe
         end
       end
-    end
   end
 end
