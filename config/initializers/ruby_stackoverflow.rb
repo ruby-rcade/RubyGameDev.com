acaf0df799efc35cf486abe523855c3c94f67424
KarenVentura <powercodegirls@gmail.com>
2015-08-19 17:59:03 -0500

    Changes names variable for Extackexchange

--- config/initializers/ruby_stackoverflow.rb
+++ config/initializers/ruby_stackoverflow.rb
@@ -1,4 +1,4 @@
 RubyStackoverflow.configure do|config|
   config.client_key = ENV["STACKEXCHANGE_CLIENT_KEY"]
-  config.access_token = ENV["ACCESS_TOKEN"]
+  config.access_token = ENV["STACKEXCHANGE_ACCESS_TOKEN"]
 end
