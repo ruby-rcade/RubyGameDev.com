5c3e6d1f939f3ca6a66054475dea2caf95c060e0
Andrew Havens <andrewh@copiousinc.com>
2015-08-20 01:28:41 -0700

    Revert development mailer config changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,15 +14,8 @@ Rubygamedev::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "localhost",
-    port: 25,
-    domain: "gmail.com",
-    authentication: "plain",
-  }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: "rubygamedev.dev" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
