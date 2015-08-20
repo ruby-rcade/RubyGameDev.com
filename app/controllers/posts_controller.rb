e6652ce4e302cb1d499847e741426f9cb37d3fcd
Andrew Havens <andrewh@copiousinc.com>
2015-08-20 01:20:42 -0700

    Remove debugging statements

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -83,8 +83,6 @@ class PostsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
-    puts "#{@post.class} #{@post.type}"
-    @post
   end
 
   def set_type
