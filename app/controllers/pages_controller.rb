e34712b7759f74a6318115c188c1bf98f7b87293
KarenVentura <powercodegirls@gmail.com>
2015-08-24 16:05:11 -0500

    Undo some changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,5 @@ class PagesController < ApplicationController
     @posts = Post.order('created_at desc')
     @library_categories = LibraryCategory.all
     render layout: 'one_column'
-    flash[:notice] = t(:hello_flash)
   end
 end
