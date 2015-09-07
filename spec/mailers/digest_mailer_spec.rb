87facd611923ae34de1a2b58905fe34118fc8cb1
KarenVentura <powercodegirls@gmail.com>
2015-09-07 17:16:25 -0500

    Fix rspec for refactor digest mailer

--- spec/mailers/digest_mailer_spec.rb
+++ spec/mailers/digest_mailer_spec.rb
@@ -1,49 +1,13 @@
 require "rails_helper"
 
 describe DigestMailer do
-  describe "#daily_digest" do
+  describe "#digest_mailer" do
     let(:user) { FactoryGirl.create(:user) }
-    let(:history) { FactoryGirl.create(:digest_history, frequency: "daily") }
-    let(:mail) { DigestMailer.daily_digest(history, user.id) }
+    let(:history) { FactoryGirl.create(:digest_history, frequency: user.digest_frequency) }
+    let(:mail) { DigestMailer.digest_mailer(history, user.id) }
 
     it "renders the subject" do
-      expect(mail.subject).to eql('RubyGameDev.com Daily Digest')
-    end
-
-    it "renders the receiver email" do
-      expect(mail.to).to eql([user.email])
-    end
-
-    it "renders the sender email" do
-      expect(mail.from).to eql(["no-reply@rubygamedev.com"])
-    end
-  end
-
-  describe "#weekly_digest" do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:history) { FactoryGirl.create(:digest_history, frequency: "weekly") }
-    let(:mail) { DigestMailer.weekly_digest(history, user.id) }
-
-    it "renders the subject" do
-      expect(mail.subject).to eql('RubyGameDev.com Weekly Digest')
-    end
-
-    it "renders the receiver email" do
-      expect(mail.to).to eql([user.email])
-    end
-
-    it "renders the sender email" do
-      expect(mail.from).to eql(["no-reply@rubygamedev.com"])
-    end
-  end
-
-  describe "#monthly_digest" do
-    let(:user) { FactoryGirl.create(:user) }
-    let(:history) { FactoryGirl.create(:digest_history, frequency: "monthly") }
-    let(:mail) { DigestMailer.monthly_digest(history, user.id) }
-
-    it "renders the subject" do
-      expect(mail.subject).to eql('RubyGameDev.com Monthly Digest')
+      expect(mail.subject).to eql("RubyGameDev.com #{user.digest_frequency} digest")
     end
 
     it "renders the receiver email" do
