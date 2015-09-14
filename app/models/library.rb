74fbacd1e90903ef0c934cb1cc59ea2e8a969182
KarenVentura <powercodegirls@gmail.com>
2015-09-14 16:06:39 -0500

    Change the validates for instruction updated

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,10 +1,10 @@
 class Library < ActiveRecord::Base
   belongs_to :category, class_name: 'LibraryCategory', foreign_key: "category_id"
   belongs_to :user
-  validates_presence_of :category
-  validates_presence_of :user
+  validates :category, presence: true
+  validates :user, presence: true
 
-  # after_create :notify_twitter
+  after_create :notify_twitter
 
   # TODO: move this to background job
   def notify_twitter
