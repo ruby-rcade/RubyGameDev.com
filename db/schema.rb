09c3a1c5af72c1bd2b7b7e365a2eeff50c3541ce
Boryana Manolova <b.manolova@yahoo.com>
2015-09-17 11:52:37 +0300

    Remove deleted table

--- db/schema.rb
+++ db/schema.rb
@@ -85,11 +85,6 @@ ActiveRecord::Schema.define(version: 20150915104923) do
     t.integer  "user_id"
   end
 
-  create_table "guides", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "libraries", force: true do |t|
     t.string   "name"
     t.text     "description"
