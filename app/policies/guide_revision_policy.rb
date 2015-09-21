d6a2d5efd5bd9237cc915fa9adb422927420d1bd
Sophia Takeva <sofy@bitex.bg>
2015-09-21 20:43:52 +0300

    Added file guide_revision policy

---
+++ app/policies/guide_revision_policy.rb
@@ -0,0 +1,11 @@
+GuideRevisionPolicy = Struct.new(:user, :post) do
+  def update?
+    return false unless user
+    user.admin? || post.user == user
+  end
+
+  def destroy?
+    return false unless user
+    user.admin?
+  end
+end
