d237b6bd567aa1fe5524e3564a14541bda59173e
KarenVentura <powercodegirls@gmail.com>
2015-09-22 17:34:27 -0500

    Refactor digest_mailer and add new method

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -3,18 +3,21 @@ class DigestMailer < ApplicationMailer
   default template_name: 'digest_mailer'
 
   def digest_mailer(history, user_id)
-    # @discussions = history.discussions
-    # @tutorials = history.tutorials
-    # @libraries = history.libraries
-    # @new_users_count = User.count - history.users_count
-    # @comment = Comment.find comment_id
-    @internal_post = InternalPost.all
     @user = User.find user_id
+    @posts = history.posts
     @history = history
     attachments.inline["header_#{history.frequency}.png"] =
       File.read("#{Rails.root}/app/assets/images/header_#{history.frequency}.png")
     attachments.inline['footer.png'] =
       File.read("#{Rails.root}/app/assets/images/footer.png")
     mail(to: @user.email, subject: "RubyGameDev.com #{history.frequency} digest")
+  end
+
+  def self.show_post(post)
+    if post.external_post?
+      post.body_html.html_safe
+    else
+      post.body_markdown
+    end
   end
 end
