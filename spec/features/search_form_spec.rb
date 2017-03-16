8d51f9f2412065b067d8e436d14f0c5676c22195
Andrew Havens <email@andrewhavens.com>
2017-03-15 22:16:18 -0700

    Fix broken test due to missing selector.

--- spec/features/search_form_spec.rb
+++ spec/features/search_form_spec.rb
@@ -8,7 +8,7 @@ describe "Search form" do
   it "checks if a specific word can be found in the posts title" do
     @post = FactoryGirl.create(:internal_post, title: 'something about rails')
     fill_in 'Search', with: 'rails'
-    find('.submit-search').click
+    click_button 'Search'
     expect(page).to have_content 'something about rails'
   end
 end
